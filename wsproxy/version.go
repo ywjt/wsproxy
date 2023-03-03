108a8ab2970248af664335b057b7cdef26ab1a65
sunshinekoo <350311204@qq.com>
2023-03-03 19:02:21 +0800

    Create version.go

---
+++ wsproxy/version.go
@@ -0,0 +1,10 @@
+// Copyright 2023 The WebSocket Proxy Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// @Author: YWJT / ZhiQiang Koo
+// @Modify: 2023-02-21   
+//
+
+package main
+
+var __VERSION__ = "2.0.0 beta"
